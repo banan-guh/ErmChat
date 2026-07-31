1ece0c58cdf74f73966a4b7dc0cb832fe4dfbfbc
banan <206313076+banan-guh@users.noreply.github.com>
2026-07-30 18:49:14 -0700

    fix: there is absolutely no way dude

--- lib/twitch_config.dart
+++ lib/twitch_config.dart
@@ -2,7 +2,7 @@ class TwitchConfig {
   static const String clientId = 'hn6tq8xvgzx91n4mx72573o1c2x9nk';
 
   static const String redirectUri =
-      'https://banan-guh.github.io/twitch-app-oauth/';
+      'https://banan-guh.github.io/twitch-app-oauth';
 
   static const String callbackUrlScheme = 'ermchat';
 
