d56b635063d38f12fd7f162b3f2d1c7dcf0806ec
banan <206313076+banan-guh@users.noreply.github.com>
2026-08-08 06:18:15 -0700

    change workflow, v0.5.8

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.LibraryExtension
+
 allprojects {
     repositories {
         google()
@@ -17,6 +19,24 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+// Strip GNU build IDs from the jni native library so reproducible-build
+// verification against GitHub Actions builds succeeds.
+subprojects {
+    plugins.withId("com.android.library") {
+        if (name == "jni") {
+            extensions.configure<LibraryExtension>("android") {
+                defaultConfig {
+                    externalNativeBuild {
+                        cmake {
+                            arguments += "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,--build-id=none"
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
