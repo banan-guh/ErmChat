2a38d6715e71bb8245ced1384989b27d925ecc90
banan <206313076+banan-guh@users.noreply.github.com>
2026-08-22 09:01:32 -0700

    chore: trim unused oauth scopes

--- test/unit/token_expiry_test.dart
+++ test/unit/token_expiry_test.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:ermchat/services/twitch_auth.dart';
